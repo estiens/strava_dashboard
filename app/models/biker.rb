5c8d0ab3151ebcae572f1ed256b78e9088dd6b04
Eric Stiens <estiens@gmail.com>
2014-08-29 19:48:19 -0500

    add some scopes

--- app/models/biker.rb
+++ app/models/biker.rb
@@ -1,3 +1,21 @@
 class Biker < ActiveRecord::Base
   has_many :rides
+
+  def this_week_distance
+    self.rides.this_week.sum(:distance).round(2) #miles
+  end
+
+  def last_week_distance
+    self.rides.last_week.sum(:distance).round(2) #miles
+  end
+
+  def this_week_speed
+    self.rides.this_week.average(:avg_speed).to_f.round(2)
+  end
+
+  def last_week_speed
+    self.rides.last_week.average(:avg_speed).to_f.round(2)
+  end
+
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,15 @@
 class Ride < ActiveRecord::Base
   belongs_to :biker
+
+  scope :between_dates, ->(begin_date, end_date) {
+    where("datetime between ? and ?", begin_date, end_date)
+  }
+
+  def self.this_week
+    self.between_dates(Time.now.beginning_of_week, Time.now)
+  end
+
+  def self.last_week
+    self.between_dates((Time.now - 7.days).beginning_of_week, (Time.now-7.days).end_of_week)
+  end
 end
